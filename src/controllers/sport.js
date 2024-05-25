b8a255270e809fcf9b2b18011fce0258fe285081
Manish Chandolia <mchandolia35@gmail.com>
2024-05-25 21:12:18 +0530

    Solution 02: return additional data as required in Problem 2

--- src/controllers/sport.js
+++ src/controllers/sport.js
@@ -4,14 +4,19 @@ const getAllSportsToursAndMatches = async () => {
     const matches = await Sport.getAllSportsToursAndMatches();
     const res = {};
     matches.forEach(match => {
-        const { sportName, tourName, matchName } = match;
+        const { sportName, tourName, matchName, matchId, matchStartTime, matchFormat } = match;
         if (!res[sportName]) {
             res[sportName] = {};
         }
         if (!res[sportName][tourName]) {
             res[sportName][tourName] = [];
         }
-        res[sportName][tourName].push(matchName);
+        res[sportName][tourName].push({
+            matchId: matchId,
+            matchName: matchName,
+            matchStartTime: matchStartTime,
+            matchFormat: matchFormat
+        });
     });
     return res;
 }
